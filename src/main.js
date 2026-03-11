6daa7b1c7b4ab6de8d8700f5f65150fc253201ce
Shaedow2000 <shaedow2000@proton.me>
2026-03-11 20:40:55 +0000

    Implement redirection to google search.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const input = document.getElementById( 'search-input' );
 const iconsDiv = document.getElementById( 'two-icons' );
 const xmark = document.getElementById( 'x-mark' );
 
-input.addEventListener( 'keyup', () => {
+input.addEventListener( 'keyup', ( e ) => {
     const value = input.value;
 
     if ( value === '' ) {
@@ -12,5 +12,14 @@ input.addEventListener( 'keyup', () => {
     } else {
         iconsDiv.style.display = 'none';
         xmark.style.display = 'block';
+    }
+
+    if ( value !== '' && e.key === 'Enter' ) {
+        const google_search = 'https://www.google.com/search?q='
+        let search_query = input.value;
+
+        search_query = search_query.replaceAll( ' ', '+' );
+
+        location.href = `${ google_search }${ search_query }` 
     }
 } );
